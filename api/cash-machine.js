852a531c8fb510c45b47f60e759c80be0bb9932e
Thiago Costa <admin@rootbsd.info>
2019-10-26 16:28:52 -0300

    Implementado calculo do Cash Machine no método XGH

--- api/cash-machine.js
+++ api/cash-machine.js
@@ -1,6 +1,18 @@
 
 module.exports = {
     calcular(notas, valor) {
-        throw new Error("Lógica não implementada");
+        var somaNotas;
+        var listaNotas = [];
+        do {
+            listaNotas.push(notas[Math.floor(Math.random()*notas.length)]);
+            somaNotas = listaNotas.reduce((valorTotal, valorNota) => valorTotal + valorNota);
+            if (somaNotas > valor)
+                listaNotas.splice(0, 1);
+        } while (somaNotas != valor);
+        var objNotas = listaNotas.reduce((objNotas, valorNota) => {
+            objNotas[valorNota] = typeof objNotas[valorNota] == "undefined" ? 1 : objNotas[valorNota] + 1;
+            return objNotas;
+        }, {});
+        return Object.keys(objNotas).map(objKey => {return {nota: objKey, quantidade: objNotas[objKey]}; });
     }
 };
