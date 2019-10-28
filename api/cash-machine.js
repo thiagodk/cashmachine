ff1fec0336bde71c65e58e7d4424e65aaf78c1ca
Thiago Costa <admin@rootbsd.info>
2019-10-28 07:25:22 -0300

    Implementada função de calculo do Cash Machine

--- api/cash-machine.js
+++ api/cash-machine.js
@@ -1,6 +1,34 @@
 
 module.exports = {
     calcular(notas, valor) {
-        throw new Error("Lógica não implementada");
+        var calcRecurse = function(notas, valor) {
+            for (var i = 0; i < notas.length; i++)
+            {
+                var resultado = [notas[i]];
+                var novoValor = valor - notas[i];
+
+                if (!novoValor)
+                    return resultado;
+                if (novoValor > 0)
+                {
+                    var resultadoRecurse = calcRecurse(notas.slice(i), novoValor);
+
+                    if (Array.isArray(resultadoRecurse) && !resultadoRecurse.reduce((resultadoValor, valor) => resultadoValor - valor, novoValor))
+                        return resultado.concat(resultadoRecurse);
+                }
+            }
+            return null;
+        };
+        var result = calcRecurse(notas.sort((a, b) => b - a), valor);
+        if (result == null)
+            return result;
+        result = result.reduce((objResult, nota) => {
+            if (objResult[nota])
+                objResult[nota]++;
+            else
+                objResult[nota] = 1;
+            return objResult;
+        }, {});
+        return Object.keys(result).map(nota => { return {nota: parseInt(nota), quantidade: result[nota]}; });
     }
 };
